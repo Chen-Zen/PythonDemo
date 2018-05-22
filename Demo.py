b8ce0cd734da77ea7c4b45f69c264bd36db1c51a
PythonDemo <yiyufeifan9@163.com>
2018-05-22 17:29:48 +0800

    https://github.com/Chen-Zen/PythonDemo.git

---
+++ Demo.py
@@ -0,0 +1,59 @@
+
+#import keyword
+
+#print(keyword.kwlist)
+
+#print(2+2)
+
+#注释
+#a,b=0,1
+
+#while b<10:
+ #   print(b)
+ #   a,b=b,a+b
+
+#a,b,c,d,=20,5.5,True,4+3j
+
+#print(type(a),type(b),type(c),type(d))
+
+#转义
+#s='Yes,he desn\'t'
+
+#print(s)
+
+#print(s,type(s),len(s))
+
+#不转义
+#print('c:\some\name')
+
+#print(r'C:\some\name')
+
+#字符串索引
+'''word='Python'
+print(word[0],word[5])
+
+print(word[-1],word[-6])'''
+
+#列表
+'''a=['him',25,100,'her']
+print(a)
+
+a=[1,2,3,4,5]
+a+[6,7,8]
+
+print(a)
+
+a=[1,2,3,4,5]
+a[0]=9
+print(a[0])
+
+a[2:5]=[11,12,13]#新增
+# print(a)
+a[2:4]=[] #删除
+print(a)'''
+
+#Tuple(元组)
+a=(1998,2018,'physics','math')
+print(a,type(a),len(a))
+
+
