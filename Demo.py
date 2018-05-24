b12eb90f10bf561290addc75b40b2cddb42c4e30
PythonDemo <yiyufeifan9@163.com>
2018-05-24 12:05:54 +0800

    break跳出循环

--- Demo.py
+++ Demo.py
@@ -114,6 +114,26 @@ for i in range(len(a)):
     print(i, a[i])
 '''
 # 可以使用range()函数来创建一个列表
+'''
 a = list(range(5))
 for i in a:
     print(a[i])
+'''
+# break和continue语句及循环中的else子句
+# break语句可以跳出for和while的循环体。如果你从for或while循环中终止，任何对应的循环else块将不执行。
+# continue语句被用来告诉Python跳过当前循环块中的剩余语句，然后继续进行下一轮循环。
+# 循环语句可以有else子句;它在穷尽列表(以for循环)或条件变为假(以while循环)循环终止时被执行,但循环被break终止时不执行.如下查寻质数的循环例子:
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, "equals", x, "*", n//x)
+            break
+    else:
+        # 循环中没有找到元素
+        print(n, "is a prime number")
+'''
+while True:
+    pass
+'''
+    
